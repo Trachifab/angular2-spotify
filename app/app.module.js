11ac4f97add569c6750d284adbdfe985706498ac
Trachifab <fabiendorey@gmail.com>
2017-01-17 18:48:24 +0100

    Add router

--- app/app.module.js
+++ app/app.module.js
@@ -14,12 +14,13 @@ var app_component_1 = require('./app.component');
 var search_component_1 = require('./components/search/search.component');
 var navbar_component_1 = require('./components/navbar/navbar.component');
 var about_component_1 = require('./components/about/about.component');
+var app_routing_1 = require('./app.routing');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, app_routing_1.routing],
             declarations: [
                 app_component_1.AppComponent,
                 search_component_1.SearchComponent,
